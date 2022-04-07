af2a9d7baeec485fe10435cf60a1adfddc96a333
brunojeh <bruno_areda@hotmail.com>
2022-04-07 09:32:11 -0300

    Commit inicial do curso de jsp

---
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,92 @@
+package servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+import java.io.IOException;
+
+import dao.DAOLoginRepository;
+
+/*O chamado controller são as servlets*/
+@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) /*Mapeamento de URL que vem da tela*/
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
+       
+ 
+    public ServletLogin() {
+    	
+    }
+
+    /*Receve os dados pela URL em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+		doPost(request, response);	
+	}
+
+	/*Recebe os dados enviados por formulario*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		String url = request.getParameter("url");
+		
+		try {
+		
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			if (daoLoginRepository.validarAutenticacao(modelLogin)) { /*Simulando login*/
+				
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				
+				if (url == null || url.equals("null")) {
+					url = "principal/principal.jsp";
+				}
+				
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+				redirecionar.forward(request, response);
+				
+			}else {
+				
+				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+				request.setAttribute("msg", "Informe o login e a senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+			
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e a senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
